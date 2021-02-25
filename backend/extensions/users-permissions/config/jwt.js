752a618e99454babe7d6d745c458f4d2a8b456dc
Artur Shestopalov <artiikk2@gmail.com>
2021-02-26 00:10:43 +0100

    initial commit

---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'dbbc631b-327c-4977-8299-aa307e43dda5'
+};
---
+++ frontend/services/api.js
@@ -0,0 +1,10 @@
+export function getStrapiURL(path = '') {
+  return `http://localhost:1337${path}`;
+}
+
+export async function fetchAPI(path) {
+  const requestUrl = getStrapiURL(path);
+  const response = await fetch(requestUrl);
+  const data = await response.json();
+  return data;
+}
